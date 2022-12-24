d33d6dcb9c7ce258e73e160637eedea12fb3e3cc
Oscar Maciel Castillo <oscarmacielc@live.com.mx>
2022-12-23 18:13:21 -0600

    C12 Taked care of
    Learned about the dynamic obstacles for the nav mesh and the carve property

---
+++ Assets/_Main/Scripts/CoroutineClass.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CoroutineClass : MonoBehaviour {
+
+	private int counter=0;
+
+	// Use this for initialization
+	void Start () {
+		StartCoroutine("SampleRoutine");
+		//StopCoroutine("SampleRoutine");
+	}
+
+	IEnumerator SampleRoutine()
+    {
+		counter++;
+		Debug.Log("Routine Started "+counter);
+		yield return null;
+		Debug.Log("Skipped 1 frame");
+		yield return new WaitForSeconds(1f);
+		Debug.Log("Skipped 1 second and ended");
+		//yield break //to exit a routine
+	}
+}
